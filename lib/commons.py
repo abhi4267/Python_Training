3384c896d276c05044a987ea2d55b02075a4051f
Abhishek singh <abhisheksingh90627@gmail.com>
2025-05-27 14:30:47 +0530

    updated

--- lib/commons.py
+++ lib/commons.py
@@ -10,7 +10,6 @@ def compute_hcf(x, y):
 
 def compute_lcm(x, y):
 
-   # choose the greater number
    if x > y:
         greater = x
    else:
@@ -29,3 +28,41 @@ def add(x, y):
 
 def multiply(x, y):
     return x * y
+
+def subtract(x, y):
+    return x - y
+
+def divide(x, y):
+    return x / y
+
+def print_factors(x):
+    print("The factors of",x,"are:")
+    for i in range(1, x + 1):
+        if x % i == 0:
+            print(i)
+
+def recur_fibo(n):
+    if n <= 1:
+        return n
+    else:
+        return(recur_fibo(n-1) + recur_fibo(n-2))
+
+def recur_sum(n):
+    if n <= 1:
+        return n
+    else:
+        return n + recur_sum(n-1)
+    
+def recur_factorial(n):
+    if n == 1:
+        return n
+    else:
+        return n*recur_factorial(n-1)
+    
+def convertToBinary(n):
+    if n > 1:
+        convertToBinary(n//2)
+    print(n % 2,end = '')
+
+def name():
+    return "Arthur","Ethan","Peter"
--- script/my_file.py
+++ script/my_file.py
@@ -1,5 +1,18 @@
 
 
-from lib.commons import add
+from lib.commons import add,compute_hcf,compute_lcm,multiply, subtract,divide, print_factors
+
 result = add(5,3)
 print("Result:", result)
+
+print(compute_hcf(45,36))
+
+print(compute_lcm(45,180))
+
+print(multiply(4,5))
+
+print(subtract(100,50))
+
+print(divide(20,10))
+
+print( print_factors(20),end=" ")
